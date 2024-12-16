7cab40b9d2a4fb97353c9db431f8a133dc70662d
Yash Mathur <mathuryash0@gmail.com>
2024-12-16 22:55:15 +0530

    changed imports for files

--- contracts/test/SettlementToken.sol
+++ contracts/test/SettlementToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.6/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SettlementToken is ERC20 {
     constructor() ERC20("Settlement Token", "STLM") {
